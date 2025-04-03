a1cbe966490e39d299d81bf2cd4b0989ce537c1f
ammiiyyy <55713700+ammiiyyy@users.noreply.github.com>
2025-04-03 20:06:07 +0530

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
